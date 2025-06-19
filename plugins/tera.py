423568f7beeb8e861cf807917c20d885620d20c3
Wrrohit788 <154300370+Wrrohit788@users.noreply.github.com>
2025-06-19 13:13:06 +0530

    Update tera.py

--- plugins/tera.py
+++ plugins/tera.py
@@ -27,7 +27,7 @@ last_upload_col = db["terabox_lastupload"]
 
 TERABOX_REGEX = r'https?://(?:www\.)?[^/\s]*tera[^/\s]*\.[a-z]+/s/[^\s]+'
 
-COOKIE = "ndus=YzrYlCHteHuixx7IN5r0fc3sajSOYAHfqDoPM0dP" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
+COOKIE = "ndus=YeX4lCnteHuiuThgpskdaXINxl8nt7OiohfWnI2I" # add your own cookies like ndus=YzrYlCHteHuixx7IN5r0ABCDFXDGSTGBDJKLBKMKH
 
 HEADERS = {
     "Accept": "application/json, text/plain, */*",
